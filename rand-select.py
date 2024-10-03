572295d0e3f36b0d0d06a94ee25404196158b2f1
Paul Hoffman <phoffman@proper.com>
2024-10-03 16:38:01 -0700

    First round of suggestions from Paul

--- rand-select.py
+++ rand-select.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+'''
+Companion program to "A Verifiable and Reproducible Random Candidate Selection Process"
+See FUTUREURLGOESHERE for more information
+'''
+
 import hashlib
 import locale
 import select
@@ -19,7 +24,7 @@ class Hexable():
   def __str__(self):
     return self.ss
 
-# Outputs a bracketed string
+# Prints a bracketed string
 def puts(out_str):
   print("[" + out_str + "]")
 
@@ -40,23 +45,23 @@ def fill_list(name):
       " || 'Q' to quit when finished"
     ss = get_input(prompt)
 
-    if "A" == ss.upper():
+    if ss.upper() == "A":
       rv.append(Hexable(get_input("Enter " + name)))
-    elif "D" == ss.upper():
+    elif ss.upper() == "D":
       puts(str(rv.pop(get_input("Enter index of " + name + " to delete", int)-1)) + " deleted")
-    elif "L" == ss.upper():
+    elif ss.upper() == "L":
       if len(rv) == 0:
         continue
       else:
         for ii in range(len(rv)):
           puts(str(ii+1) + ": " + str(rv[ii]))
-    elif "Q" == ss.upper():
+    elif ss.upper() == "Q":
       if len(rv) > 0:
         return rv
       else:
         print(name + " list is empty")
     else:
-      puts("Invalid command")
+      puts("Invalid command")     ##### Should this exit instead?
 
 # Handles STDIN
 def handle_stdin():
@@ -75,7 +80,8 @@ def handle_stdin():
   return candidates, p_values
 
 
-# BEGIN EXECUTION
+# Main program start here
+
 if locale.getlocale()[1].upper() != 'UTF-8':
   print("Locale not UTF-8")
   exit(1)
@@ -98,9 +104,8 @@ else:
    puts("_P_ values")
    p_values = fill_list("_P_ value")
 
-
 if not len(candidates):
-  put("No candidates. Aborting.")
+  puts("No candidates. Aborting.")
   exit(1)
 
 puts("Candidates")
